9ae10533ce27d25facff1380ae2cca430d24a6e2
Bhargavi <bhargavishetty113@gmail.com>
2021-10-18 22:56:24 +0530

    loadin indicator added

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -5,18 +5,20 @@ import NavBar from "../../Component/NavBar/NavBar";
 import Pagination from "../../Component/Pagination/Pagination";
 import fetchUserDetails from "../../Redux/actions";
 import styles from "./styles.module.css";
+import PropagateLoader from "react-spinners/PropagateLoader";
 
 const Home = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [userDetailsPerPage, setUserDetailsPerPage] = useState(10);
 
   const data = useSelector((state) => state);
+  const isLoading = data.isLoading;
+
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchUserDetails());
   }, []);
- 
 
   // Get current userDetails
   const indexOfLastUserDetail = currentPage * userDetailsPerPage;
@@ -34,7 +36,14 @@ const Home = () => {
     <div>
       <NavBar />
       <h1>User Details</h1>
-      <DetailsCard userDeatials={currentUserDeatils} />
+      {isLoading ? (
+        <div className={styles.loader}>
+          <PropagateLoader color={"#11bf71"} loading={isLoading} size={20} />
+        </div>
+      ) : (
+        <DetailsCard userDeatials={currentUserDeatils} />
+      )}
+
       <Pagination
         userDetailsPerPage={userDetailsPerPage}
         totaluserDetails={
